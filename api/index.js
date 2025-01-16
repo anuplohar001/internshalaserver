e387ea34f9be204b5e2b7788130a077349192982
anuplohar001 <anuplohar001@gmail.com>
2025-01-16 19:15:02 +0530

    modify:memoryLimit

--- api/index.js
+++ api/index.js
@@ -55,7 +55,8 @@ const getAws = async (key) => {
 }
 
 app.use(bodyParser.json());
-app.use(express.json())
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cors(corsOptions))
 app.get('/', (req, res) => {
     res.send("Welcome to server ")
